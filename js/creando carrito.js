3a9d212a794e47d02d1646f94131e7cb93b16c46
Pablo Di Marco <pablodimarco7@gmail.com>
2023-05-07 18:40:45 -0300

    reading info card

--- js/creando carrito.js
+++ js/creando carrito.js
@@ -18,6 +18,28 @@ function agregarCurso(e) {
 
     // .target.classList.contains para consultar si en el elemento que estamos seleccionando tiene una clase llamada 'agregar-carrito'
     if(e.target.classList.contains('agregar-carrito')) {
-        console.log('agregando al carrito...');
+        // console.log('agregando al carrito...');
+        // parentElement selecciona el elemento padre de donde seleccionamos, en este caso agregar al carrito
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+        leerDatosCurso(cursoSeleccionado);
     }
 };
+
+// lee el contenido del html al que le dimos click y extrae la info del curso
+function leerDatosCurso(curso){
+    console.log(curso);
+
+    // creacion de un objeto con el contenido del curso que seleccionamos
+    const infoCurso = {
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+    }
+
+    // el text content extrae el texto de la etiqueta
+    // el getAttribute toma el atributo data id de la etiqueta a
+    console.log(infoCurso);
+}
+
